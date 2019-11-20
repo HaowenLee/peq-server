b37ad4148153d08ebb4d184ad6785149813440da
HaowenLee <haowenhello@gmail.com>
2019-11-20 14:54:43 +0800

    Initial commit

---
+++ src/test/java/com/example/peqserver/PeqServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.peqserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeqServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
